56eda17a78cb986aa288e2746fe8eefa6a1a32e5
Christoph Gruber <gruber.christoph42@gmail.com>
2023-04-03 17:28:16 +0200

    Fix typo

--- lua/chris/set.lua
+++ lua/chris/set.lua
@@ -20,7 +20,7 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
-vim.opt.incscearch = true
+vim.opt.incsearch = true
 
 -- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
 -- delays and poor user experience.
