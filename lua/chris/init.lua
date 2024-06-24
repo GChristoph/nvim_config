88eafa2989ab656216508402ecafb98e37f23dda
Christoph Gruber <gruber.christoph42@gmail.com>
2024-06-24 14:28:41 +0200

    Netrw config and cycle through buffers with Tab

--- lua/chris/init.lua
+++ lua/chris/init.lua
@@ -1,5 +1,3 @@
 require("chris.remap")
 require("chris.set")
 require("chris.packer")
-
-
--- lua/chris/remap.lua
+++ lua/chris/remap.lua
@@ -10,4 +10,7 @@ vim.keymap.set("n", "<leader>j", "<C-W><C-j>")
 vim.keymap.set("n", "<leader>k", "<C-W><C-k>")
 vim.keymap.set("n", "<leader>l", "<C-W><C-l>")
 
+vim.keymap.set("n", "<Tab>", vim.cmd.bnext)
+vim.keymap.set("n", "<S-Tab>", vim.cmd.bprevious)
+vim.keymap.set("n", "<leader>d", vim.cmd.bdelete)
 
