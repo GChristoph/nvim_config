e139a7012914614fe40842187cb1f0d63ec4f3d2
Christoph Gruber <gruber.christoph42@gmail.com>
2024-06-27 15:23:17 +0200

    Switch to barbar for changing buffers

--- lua/chris/remap.lua
+++ lua/chris/remap.lua
@@ -4,6 +4,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeFocus)
 vim.keymap.set("n", "<leader>pt", vim.cmd.NvimTreeToggle)
 
+-- Center when jumping around with C-d and C-u
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
@@ -12,8 +13,3 @@ vim.keymap.set("n", "<leader>h", "<C-W><C-h>")
 vim.keymap.set("n", "<leader>j", "<C-W><C-j>")
 vim.keymap.set("n", "<leader>k", "<C-W><C-k>")
 vim.keymap.set("n", "<leader>l", "<C-W><C-l>")
-
-vim.keymap.set("n", "<Tab>", vim.cmd.bnext)
-vim.keymap.set("n", "<S-Tab>", vim.cmd.bprevious)
-vim.keymap.set("n", "<leader>d", vim.cmd.bdelete)
-
