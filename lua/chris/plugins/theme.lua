34777ba3c7cb7ed4bb755270bae77cb65cf9512e
Christoph Gruber <gruber.christoph42@gmail.com>
2026-06-14 17:28:28 +0200

    Remove background transparancy

--- lua/chris/plugins/theme.lua
+++ lua/chris/plugins/theme.lua
@@ -6,7 +6,7 @@ return {
     require("gruvbox").setup({
         terminal_colors = true,
         undercurl = true,
-        underline = false,
+        underline = true,
         bold = true,
         italic = {
           strings = false,
@@ -25,7 +25,7 @@ return {
         palette_overrides = {},
         overrides = {},
         dim_inactive = false,
-        transparent_mode = true,
+        transparent_mode = false,
       })
 
     vim.cmd.colorscheme("gruvbox")
